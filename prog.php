5f92936647df7459fff5f12286698b0d519720d9
府川和樹 <kazukifukawa1012@gmail.com>
2017-10-22 23:59:27 +0900

    add file 1

---
+++ prog.php
@@ -0,0 +1,78 @@
+<?php
+echo "helloww";
+ 
+date_default_timezone_set('Asia/Tokyo');
+$week = [
+  '日', //0
+  '月', //1
+  '火', //2
+  '水', //3
+  '木', //4
+  '金', //5
+  '土', //6
+];
+$time = date('i');
+$time2= date('w');
+$weekday=$week[$time2];
+require_once __DIR__ . '/vendor/autoload.php';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+try {
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+}
+foreach ($events as $event) {
+
+if ($event instanceof \LINE\LINEBot\Event\JoinEvent) {
+    
+
+  $bot->replyMessage($event->getReplyToken(),
+    (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
+      ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message))
+      
+  );
+
+
+    
+    continue;
+  }
+
+
+
+
+
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+    error_log('Non message event has come');
+    continue;
+  }
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    error_log('Non text message has come');
+    continue;
+  }
+
+
+
+  #$bot->replyText($event->getReplyToken(), $event->getText());
+ #$profile = $bot->getProfile($event->getUserId())->getJSONDecodedBody();
+  #$message = $profile["displayName"] . "さん、おはようございます！今日も頑張りましょう！";
+  #$message2 = "今は".$time."今日の天気は雨です。傘を持っていきましょう！";
+
+  
+  $bot->replymessage($event->getReplyToken(),
+    (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
+      ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message))
+      ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message2))
+       ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message3))
+  );
+
+}
+ ?>
